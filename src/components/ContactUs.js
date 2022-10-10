b493b8389e4855968369a7085a7867fdaabf41ce
henjus <80738089+henjus@users.noreply.github.com>
2022-10-10 23:10:19 +0300

    Yksi sivuisen sovelluksen toteutus.

---
+++ src/components/ContactUs.js
@@ -0,0 +1,7 @@
+export default function ContactUs() {
+    return (
+        <div>
+            Ota yhteyttä meihin
+        </div>
+    )
+}
---
+++ src/components/NotFound.js
@@ -0,0 +1,7 @@
+export default function NotFound() {
+    return (
+        <div>
+            Tämä on sivua ei löytynyt
+        </div>
+    )
+}
